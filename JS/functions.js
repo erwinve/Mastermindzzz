35fbb775673a8eaceae4f93e208734a5ed336d15
erwinve <erwinvenneker13@gmail.com>
2018-09-21 19:36:47 +0200

    fix  list

---
+++ JS/functions.js
@@ -0,0 +1,7 @@
+function changeBlue(){
+    document.getElementById("blue").style.color = "blue";
+}
+
+function changeRed(){
+    document.getElementById("circle4").style.backgroundColor="Red";
+}
--- dashboard.php
+++ dashboard.php
@@ -5,33 +5,50 @@
    include("Includes/navbar.php");
    ?>
 <div class="blackground"></div>
+<div id="DivBtn"> <input type="button" value="Red" onclick="changeRed();">
+        <input type="button" id="btnG" value="Green" onclick="btnGreen();">
+        <input type="button" id="btnB" value="Blue" onclick="btnBlue();">
+        <input type="button" id="btnReset" value="Reset" onclick="btnReset()">
+      </div>
 <div class="gamefield">
    <div class="rows border">
-      <ul class="circledropdown">
-         <li class="horizantal">
-            <a class="nav-link" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-               <div class="circle">
-               </div>
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-               <a class="dropdown-item" href="#">Home</a>
-               <a class="dropdown-item" href="#">Profiel</a>
-               <a class="dropdown-item" href="PHP/logout.php">Uitloggen</a>
-            </div>
-   </li>
-   <li class="horizantal">
-   <div class="circle">
-   </div>
-   </li>
-   <li class="horizantal">
-   <div class="circle">
-   </div>
-   </li>
-   <li class="horizantal">
-   <div class="circle">
-   </div>
-   </li>
-   </ul>
+   <ul>
+   <a href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	<li class="horizantalcircles"></li></a>
+    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+    <a class="dropdown-item bg-blue" href="#">Blauw</a>
+          <a class="dropdown-item bg-red" href="#">Rood</a>
+          <a class="dropdown-item bg-yellow" href="#">Geel</a>
+          <a class="dropdown-item bg-green" href="#">Groen</a>
+        </div>
+
+   <a href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	<li class="horizantalcircles"></li></a>
+    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+    <a class="dropdown-item bg-blue" href="#">Blauw</a>
+          <a class="dropdown-item bg-red" href="#">Rood</a>
+          <a class="dropdown-item bg-yellow" href="#">Geel</a>
+          <a class="dropdown-item bg-green" href="#">Groen</a>
+        </div>
+
+           <a href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	<li class="horizantalcircles"></li></a>
+    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+    <a class="dropdown-item bg-blue" href="#">Blauw</a>
+          <a class="dropdown-item bg-red" href="#">Rood</a>
+          <a class="dropdown-item bg-yellow" href="#">Geel</a>
+          <a class="dropdown-item bg-green" href="#">Groen</a>
+        </div>
+
+           <a href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	<li class="horizantalcircles" id="circle4" ></li></a>
+    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a id="blue" on-click="changeBlue();" class="dropdown-item bg-blue" href="#">Blauw</a>
+          <a class="dropdown-item bg-red" href="#" onclick="changeRsdfsdfed();">Rood</a>
+          <a class="dropdown-item bg-yellow" href="#">Geel</a>
+          <a class="dropdown-item bg-green" href="#">Groen</a>
+        </div>
+</ul>
 </div>
 </div>
 </div>
